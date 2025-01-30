c6b3c388b242ce1dac1afb351d5acc498dbc468f
laincy <laincy@proton.me>
2025-01-30 04:56:21 -0500

    first working version and packaged for nixos

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "WGPU experiments";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    crane,
+    fenix,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        toolchain = fenix.packages.${system}.stable.toolchain;
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+
+        #LD_LIBRARY_PATH = "${lib.makeLibraryPath nativeBuildInputs}";
+
+        reqlibs = with pkgs; [
+          wayland
+          libxkbcommon
+          libGL
+
+          vulkan-headers
+          vulkan-loader
+          vulkan-tools
+          vulkan-tools-lunarg
+          vulkan-extension-layer
+          vulkan-validation-layers
+        ];
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+
+          nativeBuildInputs = with pkgs; [makeWrapper];
+        };
+
+        my-crate = craneLib.buildPackage (commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+            postInstall = ''
+              wrapProgram $out/bin/wgpu_test --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath reqlibs}
+            '';
+          });
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          name = "wgpu dev";
+
+          packages = [toolchain];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath reqlibs}";
+          RUST_BACKTRACE = "1";
+        };
+
+        packages.default = my-crate;
+      }
+    );
+}
