01099f4f72e6d25de89a9a931876a11b7ce93025
laincy <laincy@proton.me>
2025-01-30 18:53:42 -0500

    tuorial8_depth basic

--- src/lib.rs
+++ src/lib.rs
@@ -116,6 +116,8 @@ pub struct State<'a> {
 
     instances: Vec<Instance>,
     instance_buffer: wgpu::Buffer,
+
+    depth_texture: texture::Texture,
 }
 
 impl<'a> State<'a> {
@@ -304,6 +306,8 @@ impl<'a> State<'a> {
                 push_constant_ranges: &[],
             });
 
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_config");
+
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -333,7 +337,13 @@ impl<'a> State<'a> {
                 unclipped_depth: false,
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -382,6 +392,7 @@ impl<'a> State<'a> {
 
             instances,
             instance_buffer,
+            depth_texture,
         }
     }
 
@@ -395,6 +406,8 @@ impl<'a> State<'a> {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+
+            self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture")
         }
     }
 
@@ -440,7 +453,14 @@ impl<'a> State<'a> {
                         store: wgpu::StoreOp::Store,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: wgpu::StoreOp::Store,
+                    }),
+                    stencil_ops: None
+                }),
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
